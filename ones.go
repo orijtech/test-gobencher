eedd5a8a792b3384c894514b31906529920384f7
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-08-26 21:04:05 -0700

    countOnes: quickly compute ones if powers of 2

--- ones.go
+++ ones.go
@@ -3,6 +3,15 @@ package exhibit
 // Counts the number of ones in an unsigned integer.
 // Similar to POPCOUNT, or commonly known as Hamming distance.
 func countOnes(in uint64) (count int) {
+	if in == 0 {
+		return 0
+	}
+	if in == 1 {
+		return 1
+	}
+	if in&(in-1) == 0 {
+		return 1
+	}
 	for {
 		if in == 0 {
 			return
