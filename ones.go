7efabb8a72289b450e9b5b3c6d5a6ec257c008de
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-08-26 21:24:00 -0700

    Exhibit benchmark delays by an artificial sleep

--- ones.go
+++ ones.go
@@ -1,11 +1,14 @@
 package exhibit
 
+import "time"
+
 // Counts the number of ones in an unsigned integer.
 // Similar to POPCOUNT, or commonly known as Hamming distance.
 func countOnes(in uint64) (count int) {
 	if in == 0 {
 		return 0
 	}
+	time.Sleep(10 * time.Microsecond)
 	if in&(in-1) == 0 {
 		return 1
 	}
