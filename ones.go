5dee58fed775ed2e51ec23a8db326d7ddb33812f
Emmanuel T Odeke <emmanuel@orijtech.com>
2021-08-26 20:38:47 -0700

    Augment comment

--- ones.go
+++ ones.go
@@ -1,7 +1,7 @@
 package exhibit
 
 // Counts the number of ones in an unsigned integer.
-// Similar to POPCOUNT.
+// Similar to POPCOUNT, or commonly known as Hamming distance.
 func countOnes(in uint64) (count int) {
 	for {
 		if in == 0 {
